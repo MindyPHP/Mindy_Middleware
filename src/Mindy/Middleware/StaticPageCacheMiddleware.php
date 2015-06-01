5ab93c0f99e3a9ef9a7b86e1ef5be1d44bf05871
Max107 <max@studio107.ru>
2015-06-01 14:05:11 +0300

    dont cache admin page

--- src/Mindy/Middleware/StaticPageCacheMiddleware.php
+++ src/Mindy/Middleware/StaticPageCacheMiddleware.php
@@ -7,25 +7,41 @@ use Mindy\Helper\Alias;
 use Mindy\Http\Request;
 
 /**
- * Class StaticPageCacheMiddleware
- * @package Mindy\Middleware
+ *
+ *
+ * All rights reserved.
+ *
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 11/04/14.04.2014 16:48
  */
 class StaticPageCacheMiddleware extends Middleware
 {
     /**
-     * @var int default cache timeout 5 min
+     * @var int default cache timeout 1 hour
      */
-    public $timeout = 300000;
+    public $timeout = 3600;
 
     public function processRequest(Request $request)
     {
-        $fileName = ($request->path == '/' ? 'index' : basename($request->path)) . '.html';
-        $dir = str_replace('/', '.', ltrim(dirname($request->path), '/'));
-        $cachePath = Alias::get('App.runtime.static.' . $request->host . '.' . $dir);
-        $path = $cachePath . DIRECTORY_SEPARATOR . $fileName;
-        if (file_exists($path) && (time() - fileatime($path)) <= $this->timeout) {
-            echo file_get_contents($path);
-            Mindy::app()->end();
+        $adminUrl = Mindy::app()->urlManager->reverse('admin:index');
+        if (strpos($request->path, $adminUrl) !== 0) {
+            $fileName = ($request->path == '/' ? 'index' : basename($request->path)) . '.html';
+            $dir = str_replace('/', '.', ltrim(dirname($request->path), '/'));
+            $cachePath = Alias::get('App.runtime.static.' . $request->host . '.' . $dir);
+            $path = $cachePath . DIRECTORY_SEPARATOR . $fileName;
+            if (file_exists($path)) {
+                $timestamp = fileatime($path);
+                $request->setExpires($timestamp + $this->timeout);
+                $request->addLastModified($timestamp);
+
+                $output = file_get_contents($path);
+                echo $output;
+                Mindy::app()->end();
+            }
         }
     }
 
@@ -40,6 +56,13 @@ class StaticPageCacheMiddleware extends Middleware
         $path = $cachePath . DIRECTORY_SEPARATOR . $fileName;
         if (!file_exists($path) || (time() - fileatime($path)) > $this->timeout) {
             file_put_contents($cachePath . DIRECTORY_SEPARATOR . $fileName, $output);
+            $this->setEtag($output);
         }
+    }
+
+    public function setEtag($output)
+    {
+        $etagValue = md5($output);
+        header('ETag: ' . $etagValue);
     }
 }
