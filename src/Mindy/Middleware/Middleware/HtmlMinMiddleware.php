e893d483daab3439bfbd4e6c77bd78ba38175c17
Max <max@studio107.ru>
2016-03-17 14:29:41 +0300

    raw

---
+++ src/Mindy/Middleware/Middleware/HtmlMinMiddleware.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Mindy\Middleware\Middleware;
+
+use Mindy\Http\Request;
+
+/**
+ *
+ *
+ * All rights reserved.
+ *
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 11/04/14.04.2014 16:48
+ */
+class HtmlMinMiddleware extends Middleware
+{
+    /**
+     * Be careful. This function required too many resources.
+     * Use this only with StaticPageCacheMiddleware
+     * @param Request $request
+     * @param string $output
+     */
+    public function processView(Request $request, &$output)
+    {
+        $re = '%# Collapse ws everywhere but in blacklisted elements.
+        (?>             # Match all whitespaces other than single space.
+          [^\S ]\s*     # Either one [\t\r\n\f\v] and zero or more ws,
+        | \s{2,}        # or two or more consecutive-any-whitespace.
+        ) # Note: The remaining regex consumes no text at all...
+        (?=             # Ensure we are not in a blacklist tag.
+          (?:           # Begin (unnecessary) group.
+            (?:         # Zero or more of...
+              [^<]++    # Either one or more non-"<"
+            | <         # or a < starting a non-blacklist tag.
+              (?!/?(?:textarea|pre)\b)
+            )*+         # (This could be "unroll-the-loop"ified.)
+          )             # End (unnecessary) group.
+          (?:           # Begin alternation group.
+            <           # Either a blacklist start tag.
+            (?>textarea|pre)\b
+          | \z          # or end of file.
+          )             # End alternation group.
+        )  # If we made it here, we are not in a blacklist tag.
+        %ix';
+        $output = preg_replace($re, " ", $output);
+    }
+}
---
+++ src/Mindy/Middleware/Middleware/Middleware.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Mindy\Middleware\Middleware;
+
+use Exception;
+use Mindy\Http\Request;
+
+/**
+ *
+ *
+ * All rights reserved.
+ *
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 11/04/14.04.2014 16:46
+ */
+abstract class Middleware implements IMiddleware
+{
+    /**
+     * @param Request $request
+     */
+    public function processRequest(Request $request)
+    {
+
+    }
+
+    /**
+     * Event owner RenderTrait
+     * @param \Mindy\Http\Request $request
+     * @param $output string
+     */
+    public function processView(Request $request, &$output)
+    {
+    }
+
+    /**
+     * @param Exception $exception
+     */
+    public function processException(Exception $exception)
+    {
+
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function processResponse(Request $request)
+    {
+    }
+}
