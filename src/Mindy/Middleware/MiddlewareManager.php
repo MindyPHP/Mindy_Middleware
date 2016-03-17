a98a5e8684fad1b9525054f2aa5413627ed2338e
Max <max@studio107.ru>
2016-03-17 14:29:31 +0300

    raw

--- src/Mindy/Middleware/MiddlewareManager.php
+++ src/Mindy/Middleware/MiddlewareManager.php
@@ -7,22 +7,28 @@ use Mindy\Helper\Creator;
 use Mindy\Helper\Traits\Accessors;
 use Mindy\Helper\Traits\Configurator;
 use Mindy\Http\Request;
+use Mindy\Middleware\Middleware\IMiddleware;
 
 /**
- * Class MiddlewareManager
- * @package Mindy\Middleware
+ * All rights reserved.
+ *
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 11/04/14.04.2014 16:47
  */
 class MiddlewareManager implements IMiddleware
 {
     use Configurator, Accessors;
 
     /**
-     * @var Middleware[]
+     * @var array[]
      */
     public $middleware = [];
-
     /**
-     * @var Middleware[]
+     * @var \Mindy\Middleware\Middleware\Middleware[]
      */
     private $_middleware = [];
 
@@ -33,6 +39,10 @@ class MiddlewareManager implements IMiddleware
         }
     }
 
+    /**
+     * @param Request $request
+     * @param string $output
+     */
     public function processView(Request $request, &$output)
     {
         foreach ($this->_middleware as $middleware) {
@@ -40,6 +50,9 @@ class MiddlewareManager implements IMiddleware
         }
     }
 
+    /**
+     * @param Request $request
+     */
     public function processRequest(Request $request)
     {
         foreach ($this->_middleware as $middleware) {
@@ -58,6 +71,9 @@ class MiddlewareManager implements IMiddleware
         }
     }
 
+    /**
+     * @param Request $request
+     */
     public function processResponse(Request $request)
     {
         foreach ($this->_middleware as $middleware) {
